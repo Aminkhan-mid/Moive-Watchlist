79b605aaba8c8a73df3498e91f5c3e55e6b47bf5
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2025-09-21 17:33:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const searchInp = document.getElementById('search-input')
 const searchBtn = document.getElementById('search-button')
 const moivesContainer = document.getElementById('moives-container')
 
-
-searchBtn.addEventListener('click', async ()=>{
-    let searchValue = searchInp.value
-    const response = await fetch(`http://www.omdbapi.com/?apikey=89e5c911&s=${searchValue}&plot`)
-    const data = await response.json()
-    console.log(data)
-    moivesContainer.innerHTML = renderMoivesList(data)
+searchBtn.addEventListener("click", function(){
+    searchInp.value =  ""
 })
+// searchBtn.addEventListener('click', async ()=>{
+//     const response = await fetch(`http://www.omdbapi.com/?apikey=89e5c911&s=${searchValue}&plot`)
+//     const data = await response.json()
+//     console.log(data)
+//     moivesContainer.innerHTML = renderMoivesList(data)
+// })
 
 
 function renderMoivesList(moives){
