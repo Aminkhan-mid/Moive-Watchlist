646273499fd271bc12677cb116725b7bb0fa6f22
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2025-09-21 17:30:00 +0530

    made it phone and desktop responsive

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const searchInp = document.getElementById('search-input')
 const searchBtn = document.getElementById('search-button')
+const moivesContainer = document.getElementById('moives-container')
 
 
 searchBtn.addEventListener('click', async ()=>{
     let searchValue = searchInp.value
-    const response = await fetch(`http://www.omdbapi.com/?apikey=89e5c911&s=${searchValue}`)
+    const response = await fetch(`http://www.omdbapi.com/?apikey=89e5c911&s=${searchValue}&plot`)
     const data = await response.json()
     console.log(data)
-    
+    moivesContainer.innerHTML = renderMoivesList(data)
 })
 
+
+function renderMoivesList(moives){
+    let html = ''
+    for(let moive of moives){
+      return  html =    
+    `
+        <img src="${moive.Poster}" alt="moive-poster">
+        <p>${moive.Title}</p>
+        <p>${moive.Year}</p>
+        <p>${moive.Type}</p>
+    `
+    }
+
+}
+
+// api key 89e5c911
 
