64752de7459363fbe75e664e6d0c77ba4388624e
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2025-09-22 11:18:22 +0530

    search using enter key

--- index.js
+++ index.js
@@ -7,20 +7,30 @@ searchBtn.addEventListener('click', function(){
     getMoives(searchInp.value)
 })
 
+addEventListener("keyup", function(e){
+    e.preventDefault()
+    if(e.keyCode === 13){
+        getMoives(searchInp.value)
+    }
+})
+
 async function getMoives(searchValue) {
-    const searchRes = await fetch(`https://www.omdbapi.com/?apikey=89e5c911&s=${searchValue}`)
-    const searchData = await searchRes.json()
-    if(searchData.Response === "False") {
+   const searchResponse = await fetch(`https://www.omdbapi.com/?apikey=89e5c911&s=${searchValue}`)
+   const searchData = await searchResponse.json()
+
+   if(searchData.Response === "False"){
         document.getElementById('hideHome').style.display = "none"
         document.getElementById('unavailable').innerHTML = 
         `<p>Unable to find what you’re looking for. Please try another search.</p>`
         return []
-    }
-    const movies = await Promise.all(
-        searchData.Search.map(async (movie)=>{
-            const detailsRes = await fetch(`https://www.omdbapi.com/?apikey=89e5c911&i=${movie.imdbID}&plot=short`)
-            const detailsData = await detailsRes.json()
-             const movieDetails = {
+   }
+
+   const movies = await Promise.all(
+        searchData.Search.map(async (movie) => {
+            const detailsResponse = await fetch(`https://www.omdbapi.com/?apikey=89e5c911&i=${movie.imdbID}&plot=short`)
+            const detailsData = await detailsResponse.json()
+
+            const movieDetails = {
                 title: detailsData.Title,
                 poster: detailsData.Poster,
                 runtime: detailsData.Runtime,
@@ -28,36 +38,37 @@ async function getMoives(searchValue) {
                 desc: detailsData.Plot,
                 ratings: detailsData.imdbRating
             } 
-
-                 document.getElementById('display-movies').innerHTML += `
-       <div class="moive-container">
-        <img src="${movieDetails.poster}" alt="movie photo">
-        <div class="moiveChild-container">
-            <div class="divFlex">
-                <h3>${movieDetails.title}</h3>
-                <p class="ratings">
-                    <i class="fa-solid fa-star"></i> 
-                    ${movieDetails.ratings}
-                </p>
-            </div>
-            <div class="divFlex">
-                <p class="total-mins">${movieDetails.runtime}</p>
-                <p class="genres">${movieDetails.genres}</p>
-                <button>
-                    <i class="fa-solid fa-circle-plus"></i>
-                    Watchlist
-                </button>
-            </div>
-            <p class="moive-decs">${movieDetails.desc}</p>
-        </div>  
-    </div>
-    <hr>`
-      document.getElementById('hideHome').style.display = 'none'
+            document.getElementById('display-movies').innerHTML += `
+      <div class="moive-container">
+       <img src="${movieDetails.poster}" alt="movie photo">
+       <div class="moiveChild-container">
+           <div class="divFlex">
+               <h3>${movieDetails.title}</h3>
+               <p class="ratings">
+                   <i class="fa-solid fa-star"></i> 
+                   ${movieDetails.ratings}
+               </p>
+           </div>
+           <div class="divFlex">
+               <p class="total-mins">${movieDetails.runtime}</p>
+               <p class="genres">${movieDetails.genres}</p>
+               <button>
+                   <i class="fa-solid fa-circle-plus"></i>
+                   Watchlist
+               </button>
+           </div>
+           <p class="moive-decs">${movieDetails.desc}</p>
+       </div>  
+   </div>
+   <hr>`
+    document.getElementById('hideHome').style.display = 'none'
         })
     )
     return movies
 }
-
+        
 // api key 89e5c911
 
  
+
+
